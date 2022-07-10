64f6d087148082c2f5126a18da0cead2c06e4b91
randjelovicnadja <nadja.n.randjelovic@gmail.com>
2022-07-10 16:31:28 +0200

    Dodat header

---
+++ header.php
@@ -0,0 +1,27 @@
+<header id="glavni-header-section">
+  <div class="container">
+    <div class="nav-header">
+      <a href="#" class="js-glavni-nav-toggle glavni-nav-toggle"><i></i></a>
+      <h1 id="glavni-logo"><a href="index.php"><i class="icon-aid-kit"></i>MedicaMassage</a></h1>
+      <nav id="glavni-menu-wrap" role="navigation">
+        <ul class="sf-menu" id="glavni-primary-menu">
+          <li>
+            <a href="index.php">Ponuda</a>
+          </li>
+          <li>
+            <a href="masazaDodavanje.php">Dodaj u ponudu</a>
+          </li>
+          <li>
+            <a href="administracija.php">Brisanje i izmena ponude</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
+</div>
+<aside id="glavni-hero" class="js-fullheight">
+<div class="container-fluid">
+  <img src="images/masaze.jpg" class="img img-responsive">
+</div>
+</aside>
