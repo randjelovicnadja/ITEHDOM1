2338dae82deb153accba200bc6764e49dec3f769
randjelovicnadja <nadja.n.randjelovic@gmail.com>
2022-07-10 16:32:14 +0200

    Dodat footer

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer>
+  <div id="footer">
+    <div class="container">
+      <div class="col-md-12">
+        <h1 class="text-center  footer-text">MedicaMassage</h1>
+      </div>
+      <div class="row copy-right">
+        <div class="col-md-6 col-md-offset-3 text-center">
+          <p class="glavni-social-icons">
+            <a href="#"><i class="icon-twitter2"></i></a>
+            <a href="#"><i class="icon-facebook2"></i></a>
+            <a href="#"><i class="icon-instagram"></i></a>
+            <a href="#"><i class="icon-youtube"></i></a>
+          </p>
+            <p style="color: #000000"><b>&copy; 2020 MedicaMassage by Marijana Milošević</b> </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
